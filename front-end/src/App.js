720fb4de2ea1cf3110b60d4a213896de508d6c0e
Thomas Derr <tderr@sundevils.asu.edu>
2023-11-01 18:45:39 -0700

    made page structure and added objects to get forms going

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import Navbar from './components/navBar';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
+    
+import PersonaPage from './pages/personaPage';
+import PersonasPage from './pages/personasPage';
+import StoryPage from './pages/storyPage';
+import StoriesPage from './pages/storiesPage';
+import TestPage from './pages/testPage';
+import HomePage from './pages/homePage';
 
 
-import BookQuery from './query';
 function App() {
-  return (
-    <div className="App">
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+          <Route exact path='/home' element={<HomePage />} />
+          <Route exact path='/stories' element={<StoriesPage />} />
+          <Route exact path='/personas' element={<PersonasPage />} />
+          <Route exact path='/test' element={<TestPage />} />
+         
+            </Routes>
+      </Router>
+      
 
-
-        <BookQuery/>
-
-    </div>
-  );
+      
+    );
 }
-
+ 
 export default App;
