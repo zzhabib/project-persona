a795a0527d0aba612388fa218fff0416fd3e2325
Thomas Derr <tderr@sundevils.asu.edu>
2023-11-04 17:23:48 -0700

    now can add persona, need to get page reload working

--- front-end/src/pages/personaPage.js
+++ front-end/src/pages/personaPage.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import Popup from "reactjs-popup";
 
 
-
+import { useMutation } from "@apollo/client";
 
 
 
@@ -27,31 +27,42 @@ mutation CreatePersona($input: PersonaInput!) {
 }
 `;
 
-/*
-const CHANGE_PERSONA  = gql`
-  //need to develop query that grabs id from personaName input, then updates persona based on how the form has been
-  //changed on submit
-`;
 
-*/
+function AddPersona(name, description) {
+  const { data } = useQuery(CREATE_PERSONA,
+    {
+      variables: {
+        description,
+        name
+      }
+    }
+  );
+
+  return data;
+
+}
 
 
 
-export default function PersonaPage({personaName}) {
 
 
-    const [formData, setFormData] = useState({
-        name: "",
-        description: "",
-        stories: [],
-    });
+export default function PersonaPage({ personaName }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    stories: [],
+  });
   
   console.log(personaName);
   const name = personaName;
-    const { loading, error, data } = useQuery(GET_EXISTING_STORIES, {
-      variables: { name }
-    });
+
+  const { loading, error, data } = useQuery(GET_EXISTING_STORIES, {
+    variables: { name }
+  });
   
+
+  const [createPersona] = useMutation(CREATE_PERSONA);
+   
   console.log(data);
 
 
@@ -72,29 +83,29 @@ export default function PersonaPage({personaName}) {
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };//this is jsut constantly updating the data to handle it, so I think this stays the same
+  };//this is just constantly updating the data to handle it, so I think this stays the same
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    
+      
     alert("Updated Data, please click somewhere else to close");
-    //alert(`Name: ${formData.name}, Description: ${formData.description}, Stories: ${formData.stories}`);//Instead of alerting we want to call a mutate query to the database here -- need to call different queries depending on if personaName entered
-
 
     if (!loading && !error && data && data.getPersonaByName) {
     
-
     }//if this is for an existing data point we want to mutate that data point based on Id
     else {
-
+      await createPersona({
+        variables: {
+          input: {
+            name: formData.name,
+            description: formData.description,
+          },
+        },
+      });
 
 
     }//in this case it is making a new persona
-    
-
   };
-
-
 
 
   return (
@@ -115,7 +126,3 @@ export default function PersonaPage({personaName}) {
 
   );
 }
-
-//ideally here, on the formload, we want to load the users existing stories
-//into a selection box for the stories then the user can just select
-//for now since cant query, just gonna make it text option
