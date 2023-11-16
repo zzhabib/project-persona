9ba9908d7429b86ad245aca94ab375fec89d7e20
Zach Habib <zach.habib16@gmail.com>
2023-11-16 11:06:31 -0700

    set up MUI and react router

---
+++ front-end/src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { IconButton } from '@mui/material';
+
+const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <img src="/pp-icon.png" alt="Project Persona" style={{ height: '40px' }} />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Project Persona
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
---
+++ front-end/src/routes/LoginPage.tsx
@@ -0,0 +1,8 @@
+function LoginPage() {
+
+  return <>
+    <h1>Login page</h1>
+  </>
+}
+
+export default LoginPage
