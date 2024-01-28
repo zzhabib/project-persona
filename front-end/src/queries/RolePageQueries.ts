f4c6d8c70c3fdf282310980edf0337b27dab2501
Thomas Derr <tderr@sundevils.asu.edu>
2024-01-27 22:17:00 -0700

    All functional components of the front end have been implemented in some waygit add -Agit add -A!

--- front-end/src/queries/RolePageQueries.ts
+++ front-end/src/queries/RolePageQueries.ts
@@ -5,10 +5,17 @@ import { gql } from "@apollo/client"
 export const GET_ROLE_DATA = gql`
 query Roles($getSceneId: Int!) {
     getScene(id: $getSceneId) {
+      story {
+        id
+      }
       roles {
         actions {
           name
           id
+        }
+        persona {
+          id
+          name
         }
         description
       }
@@ -22,4 +29,21 @@ mutation UpdateRole($input: RoleUpdateInput!, $sceneId: Int!, $personaId: Int!) 
     updateRole(input: $input, sceneId: $sceneId, personaId: $personaId)
   }
   
+`
+
+
+export const CREATE_ACTION = gql`
+mutation CreateAction($input: ActionInput!) {
+  createAction(input: $input) {
+    id
+    name
+  }
+}
+`
+
+
+export const DELETE_ACTION = gql`
+mutation DeleteAction($deleteActionId: Int!) {
+  deleteAction(id: $deleteActionId)
+}
 `
--- front-end/src/routes/RolePage.tsx
+++ front-end/src/routes/RolePage.tsx
@@ -2,7 +2,13 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material"
 import BackButton from "../components/BackButton";
 import { useParams } from "react-router";
-import { UPDATE_ROLE, GET_ROLE_DATA } from "../queries/RolePageQueries";
+import { UPDATE_ROLE, GET_ROLE_DATA, CREATE_ACTION, DELETE_ACTION } from "../queries/RolePageQueries";
+import { useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
+import { sectionPadding, cardStyle } from "../styles/styles";
+import CreateCard from "../components/CreateCard";
+import IdentityCard from "../components/IdentityCard";
+
 
 
 type RolePageParams = {
@@ -19,12 +25,113 @@ const RolePage: React.FC = () => {
   const personaIdNumber = personaId ? parseInt(personaId, 10) : 0;
 
 
+
+
+  const [descInput, setDescInput] = useState<string>("");
+
   // get role data function call
+  const { data, loading, refetch } = useQuery(
+    GET_ROLE_DATA, {
+      variables: { 
+        getSceneId: sceneIdNumber
+      }
+    })
+
+    const roles = data?.getScene.roles
+   
+    const filteredRoles = roles ? roles.filter((role) => role.persona.id === personaIdNumber) : [];
+
+    const myRole = filteredRoles[0]
+
+    const desc = myRole?.description || null;
+
+    const descValue = descInput != "" ? descInput : desc
+  
+    const isDirty = (descValue != desc) ? true : false
+
+    const storyId = data?.getScene.story.id
 
   // update role data function call
+  const [updateRole] = useMutation(UPDATE_ROLE, {
+    refetchQueries: [{ query: GET_ROLE_DATA, variables: { getSceneId: sceneIdNumber } }]
+  })
+
+
+const [createAction] = useMutation(CREATE_ACTION, {
+  refetchQueries: [{ query: GET_ROLE_DATA, variables: { getSceneId: sceneIdNumber } }]
+})
+
+const [deleteAction] = useMutation(DELETE_ACTION, {
+  refetchQueries: [{ query: GET_ROLE_DATA, variables: { getSceneId: sceneIdNumber } }]
+})
+
+
+    const handleFieldChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+      event.preventDefault()
+  
+      const { name, value } = event.target;
+      const newValue = value === "" ? null : value;
+      setDescInput( newValue )
+    }
 
 
   // save role needs to be general because we are doing actions with creation card, and I dont care to have to do two update functions
+  const handleSave: React.MouseEventHandler<HTMLButtonElement> = async (event) => {
+    event.preventDefault()
+
+
+    await updateRole({
+      variables: {
+        sceneId: sceneIdNumber,
+        personaId: personaIdNumber,
+        input: {
+          description: descInput
+        }
+      }
+    })
+  }
+
+
+
+  const handleActionCreate = async (name: string) => {
+
+  
+    const result = await createAction({ 
+      variables: { 
+        input : {
+          name: name,
+          storyId: storyId
+        }
+      } });
+
+
+  
+    
+    const actId = result.data.createAction.id
+
+    await updateRole({
+      variables: {
+        sceneId: sceneIdNumber,
+        personaId: personaIdNumber,
+        input: {
+          addActionIds: [actId]
+        }
+      }
+    })
+
+      refetch();
+
+  }
+
+
+  const handleActionDelete = (Id: number) => {
+
+    //might be able to just delete it and not add to the persona
+    const response = deleteAction({
+      variables: { deleteActionId: Id },
+    });
+  };
+
 
   // isdirty/ all the stuff that will go with description 
 
@@ -42,8 +149,7 @@ const RolePage: React.FC = () => {
 
 
 
-
-
+  
 
 
 
@@ -76,8 +182,8 @@ const RolePage: React.FC = () => {
           justifySelf: 'end'
         }}
         variant="contained"
-        //disabled={!isDirty}
-        //onClick={handleSave}
+        disabled={!isDirty}
+        onClick={handleSave}
       >
         SAVE
       </Button>
@@ -96,8 +202,54 @@ const RolePage: React.FC = () => {
 
 
 
+    
+          <TextField
+              label="Description"
+              name="description"
+              variant="outlined"
+              multiline
+              minRows={5}
+              maxRows={10}
+              fullWidth
+              sx={sectionPadding}
+              value={descValue}
+              onChange={handleFieldChange} // handleFieldChange for description
+              InputLabelProps={{
+                shrink: descValue !== "",
+              }}
+            />
 
 
+
+          <Box>
+          <Typography
+        variant="h4"
+        sx={{
+          padding: '10px'
+        }}>
+        Actions
+      </Typography>
+      <CreateCard
+              text="Create Action"
+              placeholder="Action Name"
+              sx={cardStyle}
+              onSubmit={handleActionCreate}
+            />
+
+<Box sx={{marginTop : '1em'}}>
+          {myRole.actions.map(s => {
+              return <IdentityCard
+                key={s.id}
+                name={s.name}
+                sx={cardStyle}  
+                onDoSomethingClick={() => handleActionDelete(s.id)}
+              />
+            })}
+
+</Box>
+
+
+          </Box>
 
     </Box>
   </>
