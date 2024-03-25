61bb504b0478688ea2efbe2606c246e53e8b1696
Zach Habib <zach.habib16@gmail.com>
2024-03-25 15:35:18 -0700

    Move API URL into .env

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
 });
 
