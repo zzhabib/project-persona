ee36f7e3dec8540027aa8451b197bda2a22c1949
Zach Habib <zach.habib16@gmail.com>
2024-03-26 13:37:41 -0700

    Changed unity project to use deployed server

--- Project Persona Demo/Assets/Scripts/Persona/PersonaEnvironment.cs
+++ Project Persona Demo/Assets/Scripts/Persona/PersonaEnvironment.cs
@@ -5,6 +5,6 @@ namespace Persona
 {
     public static class PersonaEnvironment
     {
-        public static GraphQLHttpClient Client = new GraphQLHttpClient("http://localhost:4000", new NewtonsoftJsonSerializer());
+        public static GraphQLHttpClient Client = new GraphQLHttpClient("http://146.190.161.212/api", new NewtonsoftJsonSerializer());
     }
 }
